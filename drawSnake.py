14ced0137f7755a98ccd4c79a8fd79709f2b9141
printf2019 <46348233+printf2019@users.noreply.github.com>
2019-01-03 22:45:09 +0800

    Update drawSnake.py

--- drawSnake.py
+++ drawSnake.py
@@ -1,10 +1,10 @@
 import turtle
 
-def drawSnake(ead, angle, len, neckrad):
+def drawSnake(rad, angle, len, neckrad):
     for i in range(len):
         turtle.circle(rad, angle)
         turtle.circle(-rad, angle)
-    turtle,circle(rad, angle/2)
+    turtle.circle(rad, angle/2)
     turtle.fd(rad)
     turtle.circle(neckrad+1, 180)
     turtle.fd(rad*2/3)
@@ -12,7 +12,7 @@ def drawSnake(ead, angle, len, neckrad):
 def main():
     turtle.setup(1300, 800, 0, 0)
     pythonsize = 30
-    turtle.pencolor("bule")
+    turtle.pencolor("blue")
     turtle.seth(-40)
     drawSnake(40,80,5,pythonsize/2)
     
